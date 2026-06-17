fd47e3be27cca7ea6d1a2712e07b6d16457492e0
ivan-teotonio <ivanteotonio256@gmail.com>
2026-06-17 20:37:47 -0300

    feat: ajuste no validar cpf

--- app/api/auth/validate-cpf/route.ts
+++ app/api/auth/validate-cpf/route.ts
@@ -6,7 +6,7 @@ const authService = new AuthService();
 export async function POST(req: Request) {
   try {
     const { cpf } = await req.json();
-    const user = await authService.validaCpf(cpf);
+    const user = await authService.validateCpf(cpf);
     return NextResponse.json(user);
   } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 400 });
