d700e6112bcf11728a18243fe8ffd4fb717d462d
ivan-teotonio <ivanteotonio256@gmail.com>
2026-06-30 09:52:39 -0300

    Debugando o worker

--- app/api/worker/route.ts
+++ app/api/worker/route.ts
@@ -1,11 +1,18 @@
 import { NextResponse } from "next/server";
-import { processarFila } from "@/scripts/worker"; // Você precisará exportar a função do seu arquivo
+import { processarFila } from "@/scripts/worker";
 
 export async function GET() {
+  console.log("Cron Job disparado: Iniciando processamento...");
+
   try {
     await processarFila();
+    console.log("Worker: Processamento finalizado com sucesso.");
+
     return NextResponse.json({ message: "Fila processada com sucesso" });
   } catch (error) {
+    // É muito importante logar o erro real aqui para sabermos o que está acontecendo
+    console.error("Worker: Erro crítico durante o processamento:", error);
+
     return NextResponse.json(
       { message: "Erro ao processar fila" },
       { status: 500 },
